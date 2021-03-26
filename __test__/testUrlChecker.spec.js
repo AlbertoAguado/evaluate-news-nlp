12acd9f557662c0f7a74ecf8f201d9af3e3d7bf7
AlbertoAguado <a.aguadochacon@gmail.com>
2021-03-26 16:49:23 +0100

    jest Test

---
+++ __test__/testUrlChecker.spec.js
@@ -0,0 +1,21 @@
+import {urlChecker} from "../src/client/js/urlChecker.js";
+
+test('url without http', () => {
+    expect(urlChecker("www.bbc.com/news/world.co.uk")).toBe(false);
+  });
+
+test('url with http://', () => {
+    expect(urlChecker("http://www.bbc.com/news/world")).toBe(true);
+  });
+
+test('url with random number at the beginning', () => {
+    expect(urlChecker("313135465https://www.bbc.com/news/world")).toBe(false);
+  });
+
+test('url with https://', () => {
+    expect(urlChecker("https://www.bbc.com/news/world")).toBe(true);
+  });
+
+test('url with https:// and empty', () => {
+    expect(urlChecker("https://")).toBe(false);
+  });
